602e0418da883755ea7120ed4db81c43fa04b9ee
omega-800 <gshevoroshkin@gmail.com>
2023-03-26 13:41:16 +0200

    added media + home and header

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<>
+            <Header
+              title="Guten Tag! Ich bin Georgiy Shevoroshkin"
+              text="ein motivierter Schüler und leidenschaftlicher Programmierer aus der IMS Hottingen. Auf dieser Webseite sind mein Lebenslauf, meine Attestate und Projekte zu sehen. Sie finden auch viele weitere Informationen über meine Freizeitbeschäftigungen, Interessen und Fakten über mich, die auf den Bewerbungsunterlagen nicht zu sehen sind"
+              />
+            <Home />
+            </>}
+          />
+        </Routes>
+
+      </BrowserRouter>
     </div>
   );
 }
